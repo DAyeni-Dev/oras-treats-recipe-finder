191a79c9b75c16bf73b57917b36ba0e5b7ce8b05
Your Name <Oluwadamilolaayeni321@gmail.com>
2025-08-15 08:36:44 +0100

    Header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main classsName="p-4"></main>
+    
+    </div>
+
+  );
+  
+}
+
+export default App;
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header () {
+    return(
+        <header className="bg-black-500 text-white p4">
+            <h1 className="text-2x1 font-bold text-center">ORA'S Treats Recipe finder</h1>
+        </header> 
+    );
+}
+
+export default Header;
