a629583fcd5c7ccdc50f9039f64c4dfb4b7d5e50
Your Name <Oluwadamilolaayeni321@gmail.com>
2025-08-24 20:48:32 +0100

    added navigatiion to the treat bar

--- src/components/TreatBar.jsx
+++ src/components/TreatBar.jsx
@@ -1,5 +1,6 @@
 import bgImage from "../assets/images/ora-logo-white.png";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function TreatBar({ onSearch }) {
   const [query, setQuery] = useState("");
@@ -18,11 +19,51 @@ function TreatBar({ onSearch }) {
         alt="Company Logo"
         className="absolute inset-0 w-full h-full object-cover"
       />
+        <nav className="absolute top-0 left-0 w-full flex justify-between items-center p-6 z-20">
+  <h1 className="text-2xl font-bold text-white">ORA’s Treats</h1>
+  <h4 className="text-2xl mt-4 font-semibold">Recipe Finder</h4>
+  <ul className="flex list-none gap-8 text-white font-medium">
+    <li>
+      <Link to="/" className="hover:text-green-400 transition">
+        Kitchen/
+      </Link>
+    </li>
+    <li>
+      <Link to="/treats" className="hover:text-green-400 transition">
+        Treats/
+      </Link>
+    </li>
+    <li>
+      <Link to="/About" className="hover:text-green-400 transition">
+        Behind the Treats/
+      </Link>
+    </li>
+    <li>
+      <Link to="/Contact" className="hover:text-green-400 transition">
+        Join the Banquet/
+      </Link>
+    </li>
+    <li>
+      <Link to="/favorites" className="hover:text-green-400 transition">
+        Flavor Vault/
+      </Link>
+    </li>
+    <li>
+      <Link to="/Planner" className="hover:text-green-400 transition">
+        Treat Map/
+      </Link>
+    </li>
+    <li>
+      <Link to="/grocerylist" className="hover:text-green-400 transition">
+        Market Basket
+      </Link>
+    </li>
+  </ul>
+</nav>
+
 
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center px-4">
-        <h1 className="text-6xl md:text-7xl font-extrabold">ORA'S Treats</h1>
-        <h4 className="text-2xl mt-4 font-semibold">Recipe Finder</h4>
-        <h2 className="text-3xl mt-2">Discover, Plan and Savor Every Meal</h2>
+         <h2 className="text-3xl mt-2">Discover, Plan and Savor Every Meal</h2>
         <p className="mt-6 text-lg font-mono max-w-xl">
           Find delicious recipes, plan your meals, and shop smarter with ORA’s Treat Recipes.
         </p>
